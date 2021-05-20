85478a93ce02c0d9a4827546f4bf675105bec89d
Victor Hugo Cardenas <metrichugo13@gmail.com>
2021-05-20 11:51:50 -0500

    Se agrega animación de carga en pantalla de Incidentes

--- app/src/main/java/com/telcel/gsa/sisap/ui/incidents/IncidentsViewModel.kt
+++ app/src/main/java/com/telcel/gsa/sisap/ui/incidents/IncidentsViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.telcel.gsa.sisap.LoadingStatus
 import com.telcel.gsa.sisap.ui.network.IncidenceRequest
 import com.telcel.gsa.sisap.ui.network.IncidentsList
 import com.telcel.gsa.sisap.ui.network.SisapApi
@@ -11,6 +12,10 @@ import kotlinx.coroutines.launch
 import java.lang.Exception
 
 class IncidentsViewModel(private val idEmployee: String) : ViewModel() {
+
+    private val _status = MutableLiveData<LoadingStatus>()
+    val status : LiveData<LoadingStatus>
+        get() = _status
 
     private val _incidents = MutableLiveData<IncidentsList>()
     val incidents : LiveData<IncidentsList>
@@ -22,9 +27,12 @@ class IncidentsViewModel(private val idEmployee: String) : ViewModel() {
 
     private fun getIncidents(){
         viewModelScope.launch{
+            _status.value = LoadingStatus.LOADING
             try {
                 _incidents.value = SisapApi.retrofitService.getIncidents(IncidenceRequest(idEmployee))
+                _status.value = LoadingStatus.DONE
             }catch (e: Exception){
+                _status.value = LoadingStatus.ERROR
                 _incidents.value = IncidentsList(ArrayList())
             }
         }
